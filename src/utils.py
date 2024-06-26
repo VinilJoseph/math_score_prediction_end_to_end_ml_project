7756eb6a0ed793c7ea1a2615a801ec296209fbce
Vinil Joseph <viniljpk@gmail.com>
2024-06-27 02:04:03 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -26,24 +26,29 @@ def save_object(file_path, obj):
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
 
-        for model_name, model in models.items():
-            print(f"Training model: {model_name}")
-            print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-            print(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)  # Train model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+            
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[model_name] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
